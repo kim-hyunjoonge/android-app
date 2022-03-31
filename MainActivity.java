4ec375fd63886e055240a4abdf53ccb7037ccd4c
kim-hyunjoonge <102528773+kim-hyunjoonge@users.noreply.github.com>
2022-04-01 00:56:16 +0900

    갤러리
    갤러리 진입 및 선택

---
+++ MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myapplication_gallery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void displayPicture(View v) {
+        int id = v.getId();
+        LinearLayout layout = (LinearLayout) v.findViewById(id);
+        String tag = (String) layout.getTag();
+
+//        Toast.makeText(this, "클릭한 아이템" + tag, Toast.LENGTH_LONG).show();
+
+
+        Intent it = new Intent(this, Picture.class);
+        it.putExtra("it_tag", tag);
+        startActivity(it);
+        }
+    }
